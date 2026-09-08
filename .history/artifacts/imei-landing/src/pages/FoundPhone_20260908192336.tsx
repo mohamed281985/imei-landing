ee2574164fb8b35303c6b03061bae825f5723f4e
mohamed281985 <maly5424@gmail.com>
2026-09-08 19:28:58 +0300

    dfdkffvf

--- .history/artifacts/imei-landing/src/pages/FoundPhone_20260908192336.tsx
+++ .history/artifacts/imei-landing/src/pages/FoundPhone_20260908192336.tsx
@@ -454,7 +454,7 @@ export default function FoundPhone() {
                   </AnimatePresence>
 
                   <AnimatePresence>
-                    {!data.anther_number?.trim() && (
+                    {!data.anther_number && (
                       <motion.div
                         initial={{ opacity: 0, scale: 0.9 }}
                         animate={{ opacity: 1, scale: 1 }}
